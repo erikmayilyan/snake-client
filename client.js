9484c0e3a5d8e0b3fb12668ead3002bdce20dc09
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-25 15:55:24 -0700

    Modifying client js

--- client.js
+++ client.js
@@ -3,16 +3,19 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
+    host: "192.168.0.23",
     port: 50541
   });
   conn.on('data', (data) => {
     console.log(data.toString());
-    conn.end();
   });
   conn.on('end', () => {
     console.log('disconnected from server');
   });
+  conn.on('connect', () => {
+    console.log("Connected!");
+    conn.write("Name: snake");
+  })
   // interpret incoming data as text
   conn.setEncoding("utf8");
   
