4f4966bbe28c125df6a040a8d4346c4943a445f7
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-25 15:39:26 -0700

    Commit client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+  conn.on('end', () => {
+    console.log('disconnected from server');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  return conn;
+};
+
+module.exports = connect;
