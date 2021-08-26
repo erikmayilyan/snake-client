731fb42c08521206578a146a27bdd144c2840c6a
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-25 18:15:11 -0700

    Modify client js

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "192.168.0.23",
-    port: 50541
+    host: IP,
+    port: PORT
   });
   conn.on('data', (data) => {
     console.log(data.toString());
